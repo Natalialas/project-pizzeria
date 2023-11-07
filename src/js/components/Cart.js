632b242a9bc2fd6d2ec3a5ad0cd8a4929235d658
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2023-11-07 09:39:07 +0100

    Change routes to files

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,6 +1,6 @@
-import {select, classNames, templates, settings} from './settings.js';
-import CartProduct from "./Components/CartProduct.js";
-import utils from './utils.js';
+import {select, classNames, templates, settings} from '../settings.js';
+import CartProduct from "./CartProduct.js";
+import utils from '../utils.js';
 
 class Cart{
     constructor(element){
